62d84a7ff2347f65322ab09fcfdbf80ab0296a0c
jubaer-ad <jubaerad1@gmail.com>
2025-10-22 23:49:27 +0600

    Added Card Component with style

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import Food from "./Food";
-import Footer from "./Footer";
-import Header from "./Header"
+import Card from "./Card";
+
 
 function App() {
   return (
     <>
-      <Header />
-      <Food />
-      <Footer />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
     </>
   );
 }
--- src/Footer.jsx
+++
@@ -1,8 +0,0 @@
-
-function Footer() {
-    return (
-        <p>&copy;{new Date().getFullYear()} Your company name</p>
-    );
-}
-
-export default Footer
