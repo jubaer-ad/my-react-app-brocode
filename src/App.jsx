b3b67dd65f0edf24497ff7c90505e9e0dcdf5f79
jubaer-ad <jubaerad1@gmail.com>
2025-10-23 18:23:07 +0600

    clear

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,8 @@
-import Student from "./Student";
-
 
 
 function App() {
   return (
     <>
-      <Student name="Shaakira" age={45} isStudent={false} />
-      <Student name="JLO" age={49} isStudent={true} />
-      <Student />
     </>
   );
 }
