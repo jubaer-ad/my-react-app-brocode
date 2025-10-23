0fbf61143457bdab1e38d3536fc3654b1c12d904
jubaer-ad <jubaerad1@gmail.com>
2025-10-23 20:52:33 +0600

    clear

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
-import UserGreeting from "./UserGreeting";
+
 
 
 function App() {
   return (
     <>
-    <UserGreeting isLoggedIn={true} name='amanda' gg="gg"/>
-    <UserGreeting/>
-    <UserGreeting isLoggedIn={true}/>
     </>
   );
 }
